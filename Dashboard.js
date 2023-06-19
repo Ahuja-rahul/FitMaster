f9a64fdfd1baed598b6a8388030e4098e0283471
Ahuja-rahul <ahuja742@gmail.com>
2023-06-19 12:44:47 -0400

    #5 Adds new tab and Screen for Search Screen

--- Dashboard.js
+++ Dashboard.js
@@ -7,51 +7,55 @@ import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import { MaterialIcons } from '@expo/vector-icons';
 import HomeScreen from './Screens/HomeScreen';
 import MyWorkoutScreen from './Screens/MyWorkoutScreen';
+import SearchScreen from './Screens/SearchScreen'; 
+import SettingScreen from './Screens/SettingScreen';
 
 
-const SettingsScreen = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings Screen</Text>
-    </View>
-);
-
-// Create the bottom tab navigator
 const Tab = createBottomTabNavigator();
 
 const Dashboard = () => (
-    <Tab.Navigator>
-        <Tab.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-                tabBarLabel: 'Home',
-                tabBarIcon: ({ color, size }) => (
-                    <Icon name="home" color={color} size={size} />
-                )
-            }}
-        />
-        <Tab.Screen
-            name="My Workout"
-            component={MyWorkoutScreen}
-            options={{
-                tabBarLabel: 'My Workout',
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="fitness-center" size={24} color="black" />
-                )
-            }}
-        />
-        <Tab.Screen
-            name="Settings"
-            component={SettingsScreen}
-            options={{
-                tabBarLabel: 'Settings',
-                tabBarIcon: ({ color, size }) => (
-                    <Icon name="settings-outline" color={color} size={size} />
-                )
-            }}
-        />
-
-    </Tab.Navigator>
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="home" color={color} size={size} />
+        )
+      }}
+    />
+    <Tab.Screen
+      name="Search" 
+      component={SearchScreen} 
+      options={{
+        tabBarLabel: 'Search',
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="search" color={color} size={size} />
+        )
+      }}
+    />
+    <Tab.Screen
+      name="My Workout"
+      component={MyWorkoutScreen}
+      options={{
+        tabBarLabel: 'My Workout',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialIcons name="fitness-center" size={24} color="black" />
+        )
+      }}
+    />
+    <Tab.Screen
+      name="Settings"
+      component={SettingScreen}
+      options={{
+        tabBarLabel: 'Settings',
+        tabBarIcon: ({ color, size }) => (
+          <Icon name="settings-outline" color={color} size={size} />
+        )
+      }}
+    />
+  </Tab.Navigator>
 );
 
 export default Dashboard;
--- assets/StackNavigator.js
+++ assets/StackNavigator.js
@@ -1,5 +1,4 @@
 import { createStackNavigator } from '@react-navigation/stack';
-// import { NavigationContainer } from '@react-navigation/native';
 import MyWorkoutScreen from '../Screens/MyWorkoutScreen';
 import HomeScreen from '../Screens/HomeScreen';
 import Dashboard from '../Dashboard';
@@ -10,26 +9,27 @@ import Workout4Screen from '../Screens/Workout4Screen';
 import Workout5Screen from '../Screens/Workout5Screen';
 import Workout6Screen from '../Screens/Workout6Screen';
 import Banner from '../Screens/Banner';
+import SearchScreen from '../Screens/SearchScreen';
+
 
 const Stack = createStackNavigator();
 
 function MyStack() {
-    return (
-        
-          <Stack.Navigator>
-          <Stack.Screen name="Dash" component={Dashboard} options={{headerShown:false}}/>
-            <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
-            <Stack.Screen name="WorkOutScreen" component={MyWorkoutScreen} options={{headerShown:false}}/>
-            <Stack.Screen name="W1" component={Workout1Screen} options={{headerShown:false}}/>
-            <Stack.Screen name="W2" component={Workout2Screen} options={{headerShown:false}}/>
-            <Stack.Screen name="W3" component={Workout3Screen} options={{headerShown:false}}/>
-            <Stack.Screen name="W4" component={Workout4Screen} options={{headerShown:false}}/>
-            <Stack.Screen name="W5" component={Workout5Screen} options={{headerShown:false}}/>
-            <Stack.Screen name="W6" component={Workout6Screen} options={{headerShown:false}}/>
-            <Stack.Screen name="Banner" component={Banner} options={{headerShown:false}}/>
-            
-          </Stack.Navigator>
-        
-      )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Dash" component={Dashboard} options={{ headerShown: false }} />
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="WorkOutScreen" component={MyWorkoutScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="Search" component={SearchScreen} options={{ headerShown: true }} />
+      <Stack.Screen name="BackBicepB" component={Workout1Screen} options={{headerShown: true,headerBackTitleVisible: false,}}/>
+      <Stack.Screen name="BacknBicepA" component={Workout2Screen} options={{ headerShown: true, headerBackTitleVisible: false }} />
+      <Stack.Screen name="ChestnTricepB" component={Workout3Screen} options={{ headerShown: true, headerBackTitleVisible: false }} />
+      <Stack.Screen name="ChestnTricepA" component={Workout4Screen} options={{ headerShown: true, headerBackTitleVisible: false }} />
+      <Stack.Screen name="LegnShoulderB" component={Workout5Screen} options={{ headerShown: true, headerBackTitleVisible: false }} />
+      <Stack.Screen name="LegnShoulderA" component={Workout6Screen} options={{ headerShown: true, headerBackTitleVisible: false }} />
+      <Stack.Screen name="Banner" component={Banner} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
 }
-export default MyStack
+
+export default MyStack;
