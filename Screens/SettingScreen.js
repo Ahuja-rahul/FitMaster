47df2f6053abd453727cc4f3bb90977aa6217465
Krunal Parmar <51773044+krunal2598@users.noreply.github.com>
2023-06-20 14:54:00 -0400

    adds user preference #4
    adds options for user preference like  notification , dark mode etc.

--- Screens/SettingScreen.js
+++ Screens/SettingScreen.js
@@ -1,14 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View, Text, Switch, Image, Pressable } from 'react-native';
 
-const SettingScreen = () => {
-  return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
-  )
-}
+const SettingScreen = ({ navigation }) => {
+    const goToProfile = () => {
+        navigation.navigate('Profile');
+    };
 
-export default SettingScreen
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={goToProfile} style={styles.profileContainer}>
+                <Image
+                    style={styles.profileImage}
+                    source={require('../assets/icon.png')}
+                />
+                <Text style={styles.profileName}>John Doe</Text>
+            </Pressable>
+            <Text style={styles.title}>Settings</Text>
+            <Text style={styles.subtitle}>General</Text>
+            <View style={styles.settingItem}>
+                <Text>Notifications</Text>
+                <Switch />
+            </View>
+            <View style={styles.settingItem}>
+                <Text>Dark Mode</Text>
+                <Switch />
+            </View>
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+    profileContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16,
+    },
+    profileImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        marginRight: 12,
+    },
+    profileName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+    subtitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 16,
+        marginBottom: 8,
+    },
+    settingItem: {
+        paddingVertical: 12,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+});
+
+export default SettingScreen;
